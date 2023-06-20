dbe4aca55503c135e45a7d97f0ba74472f21fde3
Abhijit Barik <71961635+Abhijit-Barik01@users.noreply.github.com>
2023-06-21 00:24:35 +0530

    Create Histogram of Tweets.sql

---
+++ Histogram of Tweets.sql
@@ -0,0 +1,7 @@
+with cte as(
+SELECT user_id,COUNT(*) as tweet_bucket FROM tweets
+WHERE EXTRACT(year from tweet_date)=2022
+GROUP BY user_id)
+
+SELECT tweet_bucket,COUNT(*) as users_num from cte 
+GROUP BY tweet_bucket
